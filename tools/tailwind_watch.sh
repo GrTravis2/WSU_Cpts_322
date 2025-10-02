1785b6c950b0f30323eb74137d93d3e3a80b841f
Gavin Travis <g.r.travis.2@gmail.com>
2025-10-02 16:44:45 -0700

    changed script to accept any tailwind args and also updated README

--- tools/tailwind_watch.sh
+++
@@ -1,4 +0,0 @@
-
-#!/bin/bash
-
-tailwindcss -i ./attendance_tracker/templates/src/input.css -o ./attendance_tracker/static/style.css --watch
---
+++ tools/tailwindcss.sh
@@ -0,0 +1,4 @@
+
+#!/bin/bash
+
+tailwindcss -i ./attendance_tracker/templates/src/input.css -o ./attendance_tracker/static/style.css $@ # and whatever other args
