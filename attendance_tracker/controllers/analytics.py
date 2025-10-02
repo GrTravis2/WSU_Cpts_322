8364aa33a2694f683c91e554d0d1dff44146f908
Ingrid Llorente <143571288+lacroixmeariver@users.noreply.github.com>
2025-10-02 10:11:32 -0700

    Removing app route from controllers/analytics

--- attendance_tracker/controllers/analytics.py
+++ attendance_tracker/controllers/analytics.py
@@ -2,21 +2,10 @@
 
 from __future__ import annotations
 
-from flask import Blueprint, render_template
+from flask import Blueprint
 
 ANALYTICS = Blueprint(
     name="analytics",
     import_name=__name__,
     url_prefix="/h/analytics",
 )
-
-
-@ANALYTICS.route("/analytics")
-def analytics():
-    """Render the analytics view page.
-
-    Name and title serve as variables for templates.
-    """
-    return render_template(
-        "analytics.html", name="ANALYTICS PAGE", title="ANALYTIC VIEW"
-    )
