8572e3f2e66b9032935a208e04c40a655c142a43
Ingrid Llorente <143571288+lacroixmeariver@users.noreply.github.com>
2025-10-01 19:24:22 -0700

    Adding the UI elements, modifing .gitignore

--- attendance_tracker/controllers/analytics.py
+++ attendance_tracker/controllers/analytics.py
@@ -2,10 +2,21 @@
 
 from __future__ import annotations
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 ANALYTICS = Blueprint(
     name="analytics",
     import_name=__name__,
     url_prefix="/h/analytics",
 )
+
+
+@ANALYTICS.route("/analytics")
+def analytics():
+    """Render the analytics view page.
+
+    Name and title serve as variables for templates.
+    """
+    return render_template(
+        "analytics.html", name="ANALYTICS PAGE", title="ANALYTIC VIEW"
+    )
